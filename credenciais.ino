03d47b0cd4d11ba77dc24cc40d5d96f173555f7c
Bruno Lima <bru.no@outlook.com.br>
2024-05-14 13:18:21 -0300

    Criação do arquivo para inserir os SSIDs e senhas das redes Wi-Fi que serão utilizadas

---
+++ credenciais.ino
@@ -0,0 +1,10 @@
+void credenciaisWifi() {
+
+  // Cadastrar múltiplas redes Wi-Fi
+  wifiMulti.addAP("SSID 1", "Senha 1");
+  wifiMulti.addAP("SSID 2", "Senha 2");
+  wifiMulti.addAP("SSID 3", "Senha 3");
+  wifiMulti.addAP("SSID 4", "Senha 4");
+  wifiMulti.addAP("SSID 5", "Senha 5");
+  // ...
+}
