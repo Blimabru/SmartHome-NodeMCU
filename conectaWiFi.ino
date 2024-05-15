0e206a5d649fd5cd194940168a19ebc00c00fa9c
Blimabru <bru.no@outlook.com.br>
2024-05-15 20:50:22 -0300

    Inserção de tratamentos de erros na conexão Wi-Fi

--- conectaWiFi.ino
+++ conectaWiFi.ino
@@ -1,3 +1,7 @@
+#include <ESP8266WiFiMulti.h>
+
+ESP8266WiFiMulti wifiMulti;  // Instancia um objeto ESP8266WiFiMulti chamado wifiMulti
+
 void conectaWifi() {
 
   const uint32_t connectTimeoutMs = 10000;  // Tempo limite para conectar ao Wi-Fi.
@@ -9,7 +13,9 @@ void conectaWifi() {
   // Conecta ao Wi-Fi
   while (!conectado) {
 
-    Serial.println("Conectando ao Wi-Fi...");
+    Serial.println("Conectando ao Wi-Fi...\n");
+
+    msgConectando();
 
     credenciaisWifi();
 
@@ -17,19 +23,102 @@ void conectaWifi() {
       Serial.println("Ok.\n");
       Serial.print("Conectado a: ");
       Serial.println(WiFi.SSID());
-      Serial.print("IP local: ");
-      Serial.println(WiFi.localIP());
-      Serial.print("\n");
+      Serial.print("\nIP local: ");
+      Serial.println(WiFi.localIP() + "\n");
 
       conectado = true;
+
+      msgConectado(WiFi.SSID());
 
       break;
 
     } else {
-      Serial.print("Erro ao conectar ao Wi-Fi: ");
-      Serial.println(WiFi.status());
+
+      verificaStatusWiFi();
+
       delay(1000);
     }
   }
   delay(1000);
+}
+
+void verificaStatusWiFi() {
+
+    wl_status_t estadoWifi = WiFi.status();
+    String estado;
+
+    switch (estadoWifi) {
+
+        case WL_IDLE_STATUS:
+
+            estado = "Wi-Fi está ocioso.";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;
+
+        case WL_NO_SSID_AVAIL:
+
+            estado = "SSID não disponível.";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;
+
+        case WL_SCAN_COMPLETED:
+
+            estado = "Varredura de redes Wi-Fi concluída.";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;
+
+        case WL_CONNECTED:
+
+            estado = "Conectado ao Wi-Fi";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;
+
+        case WL_CONNECT_FAILED:
+
+            estado = "Falha na conexão.";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;
+
+        case WL_CONNECTION_LOST:
+
+            estado = "Conexão perdida.";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;
+
+        case WL_DISCONNECTED:
+
+            estado = "Desconectado do Wi-Fi.";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;;
+
+        default:
+
+            estado = "Erro Desconhecido.";
+
+            Serial.println("Erro: " + estado + "\n");
+            msgErroConexao(estado);
+
+            break;
+    }
 }
