b1e52f184a3e3833380e26edc64bf6c5baca5c50
Blimabru <bru.no@outlook.com.br>
2024-05-15 20:42:25 -0300

    Movido o local onde é incluído as bibliotecas para o arquivo .ino de suas respectivas funções

--- main.ino
+++ main.ino
@@ -1,16 +1,9 @@
-#include <ESP8266WiFiMulti.h>
-#include <LiquidCrystal_I2C.h>  // Download: www.arduino.cc/reference/en/libraries/liquidcrystal-i2c/
-
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
-ESP8266WiFiMulti wifiMulti;  // Instancia um objeto ESP8266WiFiMulti chamado wifiMulti
-
 void setup() {
-
   Serial.begin(115200);
 
   iniciaDisplay();
-  msgInicial();
   conectaWifi();
+  
 }
 
 void loop() {
