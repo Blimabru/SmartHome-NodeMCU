5fdff0e0c8f81918bad4d598902d1a421b7f7e98
Blimabru <bru.no@outlook.com.br>
2024-05-17 00:32:24 -0300

    Declaração de tempo entre as leituras de temperatura e umidade movida para o arquivo main

--- sensores.ino
+++ sensores.ino
@@ -1,15 +1,13 @@
 #include <DHT.h> //https://www.arduino.cc/reference/en/libraries/dht-sensor-library/
 
 #define DHTPIN D3     // Define o pino ao qual o sensor está conectado
-#define DHTTYPE DHT11 // Define o tipo do sensor (DHT11, DHT21, DHT22
+#define DHTTYPE DHT11 // Define o tipo do sensor (DHT11, DHT21, DHT22)
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void verificarTemperatura() {
 
   dht.begin();
-
-  delay(1000); // Aguarda 1 segundo entre as leituras
 
   float t = dht.readTemperature();  // Lê a temperatura em Celsius
 
@@ -23,8 +21,6 @@ void verificarTemperatura() {
 void verificarUmidade() {
 
   dht.begin();
-
-  delay(1000); // Aguarda 1 segundo entre as leituras
 
   float u = dht.readHumidity();     // Lê a umidade relativa
 
